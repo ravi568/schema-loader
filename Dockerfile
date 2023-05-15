21b9698896590f61b03ea3a595459871dd91cb38
ravichandra.kalluri <kalluriravi444@gmail.com>
2023-05-15 11:58:40 +0530

    schema loader creation for mongo and mysql, updating job yaml files

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM        dokken/centos-8
 RUN         yum install epel-release -y
 COPY        mongo.repo /etc/yum.repos.d/mongo.repo
-RUN         yum install git mysql mongodb-org-shell -y
+RUN         yum install unzip git jq mysql mongodb-org-shell -y
+RUN         curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install
 COPY        run.sh /
 ENTRYPOINT  [ "bash", "/run.sh" ]
--- run.sh
+++ run.sh
@@ -4,10 +4,20 @@ cd /app
 case $1 in
   mongo)
     curl -L https://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem -o /app/rds-combined-ca-bundle.pem
-    mongo --ssl --host {{ lookup('amazon.aws.aws_ssm','{{env}}.docdb.endpoint', region='us-east-1')}}:27017 --sslCAFile /app/rds-combined-ca-bundle.pem --username "{{ lookup('amazon.aws.aws_ssm', '{{env}}.docdb.user', region='us-east-1')}}" --password "{{ lookup('amazon.aws.aws_ssm', '{{env}}.docdb.pass', region='us-east-1')}}" </app/schema/{{component}}.js
+    mongo --ssl \
+    --host $(aws ssm get-parameter --name ${env}.docdb.endpoint --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g'):27017 \
+    --sslCAFile /app/rds-combined-ca-bundle.pem \
+    --username $(aws ssm get-parameter --name ${env}.docdb.user --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') \
+    --password $(aws ssm get-parameter --name ${env}.docdb.pass --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') \
+      </app/schema/${2}.js
 
     ;;
   mysql)
+    mysql -h $(aws ssm get-parameter --name ${env}.rds.endpoint --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') \
+     -u $(aws ssm get-parameter --name ${env}.rds.user --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') \
+     -p $(aws ssm get-parameter --name ${env}.rds.pass --with-decryption | jq '.Parameter.Value' | sed -e 's/"//g') \
+      </app/schema/${2}.sql
+
     ;;
   *)
     echo schema loading supported for only mongo and mysql
@@ -24,3 +34,6 @@ esac
 #    - name: Load Schema
 #      #ansible.builtin.shell: mongo --host mongodb-dev.kalluriravidevops71.online </app/schema/{{component}}.js
 #      ansible.builtin.shell: mongo --ssl --host {{ lookup('amazon.aws.aws_ssm','{{env}}.docdb.endpoint', region='us-east-1')}}:27017 --sslCAFile /app/rds-combined-ca-bundle.pem --username "{{ lookup('amazon.aws.aws_ssm', '{{env}}.docdb.user', region='us-east-1')}}" --password "{{ lookup('amazon.aws.aws_ssm', '{{env}}.docdb.pass', region='us-east-1')}}" </app/schema/{{component}}.js
+
+
+#mysql -h {{ lookup('amazon.aws.aws_ssm', '{{env}}.rds.endpoint', region='us-east-1') }} -u{{ lookup('amazon.aws.aws_ssm', '{{env}}.rds.user', region='us-east-1') }} -p{{ lookup('amazon.aws.aws_ssm', '{{env}}.rds.pass', region='us-east-1') }} < /app/schema/{{component}}.sql
